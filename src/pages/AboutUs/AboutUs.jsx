dd62dbc6f33b4c5b5384dfe984b47f95faed044b
venmorr <stevenmorrrca@email.com>
2023-10-15 16:07:45 -0400

    set up dev cards

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -9,7 +9,10 @@ const AboutUs = () => {
         <div>
             <h2>Face</h2>
             <h2>Carla Pacheco</h2>
-            <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, reprehenderit explicabo! Facere enim qui quibusdam ea ex, sapiente odio. Nam magnam voluptatibus quasi explicabo natus dolorum perferendis deserunt omnis adipisci!
+            </p>
+            <div className={styles.links}>
               <a href='https://www.linkedin.com/in/thecarlapacheco/'>LI</a>
               <a href='https://github.com/cmpacheco23'>GH</a>
             </div>
@@ -18,7 +21,10 @@ const AboutUs = () => {
           <div>
             <h2>Face</h2>
             <h2>Enes Velovic</h2>
-            <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, reprehenderit explicabo! Facere enim qui quibusdam ea ex, sapiente odio. Nam magnam voluptatibus quasi explicabo natus dolorum perferendis deserunt omnis adipisci!
+            </p>
+            <div className={styles.links}>
               <a href='https://www.linkedin.com/in/enesvelovic/'>LI</a>
               <a href='https://github.com/NSnyc'>GH</a>
             </div>
@@ -27,7 +33,10 @@ const AboutUs = () => {
           <div>
             <h2>Face</h2>
             <h2>Steve Morrison</h2>
-            <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, reprehenderit explicabo! Facere enim qui quibusdam ea ex, sapiente odio. Nam magnam voluptatibus quasi explicabo natus dolorum perferendis deserunt omnis adipisci!
+            </p>
+            <div className={styles.links}>
               <a href='https://www.linkedin.com/in/steven-ansman-morrison/'>LI</a>
               <a href='https://github.com/venmorr'>GH</a>
             </div>
