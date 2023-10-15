aa79191b6965a45f5e050fb195614a255a7420c3
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-15 19:35:08 -0400

    set up bookdetails page component

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom"
+
+import BookDetails from "../../pages/BookDetails/BookDetails"
+
 import styles from "./BookCard.module.css"
 
 const BookCard = (props) => {
@@ -9,6 +12,7 @@ const BookCard = (props) => {
         <h4>{props.book.title}</h4>
         <h5>{props.book.authors}</h5>
       </Link>
+
     </div>
   )
 }
---
+++ src/pages/BookDetails/BookDetails.jsx
@@ -0,0 +1,11 @@
+import styles from "./BookDetails.module.css"
+
+const BookDetails = () => {
+  return (
+    <main>
+
+    </main>
+  )
+}
+
+export default BookDetails;
