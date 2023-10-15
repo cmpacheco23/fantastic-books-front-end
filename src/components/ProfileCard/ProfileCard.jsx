e00658c87f09f0cc661f8fe070410a82360ed28a
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-15 14:04:42 -0400

    adjust profile information to render

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 import styles from './ProfileCard.module.css'
 
@@ -10,8 +11,11 @@ const ProfileCard = ({profile}) => {
   
   return (
     <div className="card">
-      <h1> {profile.name} </h1>
       <img src={profile.photo} alt='profile photo'/>
+      <h1> {profile.name} </h1>
+      <h4>Number of Shelves: </h4>
+      <h4>Number of Books: </h4>
+      <Link to={`/profiles/${profile._id}`}>View Profile</Link>
       {/* add number of shelves
       add number of books */}
     </div>
