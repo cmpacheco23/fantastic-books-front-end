4f4b6babe213c240d5221e72afb995d4cb4a3281
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-15 19:41:33 -0400

    pass props downt to bookdetails page component

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -4,7 +4,8 @@ import BookDetails from "../../pages/BookDetails/BookDetails"
 
 import styles from "./BookCard.module.css"
 
-const BookCard = (props) => {
+
+const BookCard = (props) => { 
   return (
     <div className={styles.linkContainer}>
       <Link to={`/books/${props.book.id}`}>
@@ -12,7 +13,7 @@ const BookCard = (props) => {
         <h4>{props.book.title}</h4>
         <h5>{props.book.authors}</h5>
       </Link>
-
+      <BookDetails book={props.book}/>
     </div>
   )
 }
