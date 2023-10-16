1cd2c34db1f48d9fab25c6660ef1d564758b000e
venmorr <stevenmorrrca@email.com>
2023-10-16 10:29:49 -0400

    fix spacing on nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,31 +6,37 @@ import styles from "./NavBar.module.css"
 const NavBar = ({ user, handleLogout }) => {
   return (
     <nav className={styles.nav}>
-      {user ?
+      
+      <ul className={styles.right}>
+        <li><NavLink to="/"> Home</NavLink></li>
+        
+        {user ? <li>Welcome, {user.name}</li> : '' }
+      </ul>
+      
+      <div className={styles.left}>
         <ul>
-          <div className={styles.right}></div>
-          <li>
-            <NavLink to="/"> Home</NavLink>
-          </li>
-
-
-          <li><NavLink to="/auth/logout" onClick={handleLogout}>Logout</NavLink></li>
-          <li>Welcome, {user.name}</li>
-
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
           <li><NavLink to="/search-books"> Search Books </NavLink></li>
+          
+          {user ? <li><NavLink to="/profiles">Profiles</NavLink></li> : '' }
+
           <li><NavLink to="/about"> About Us</NavLink></li>
+        
+        
         </ul>
-      :
-        <ul>
-          <li><NavLink to="/"> Home</NavLink></li>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-          <li><NavLink to="/search-books"> Search Books </NavLink></li>
-          <li><NavLink to="/about"> About Us</NavLink></li>
-        </ul>
-      }
+        
+        
+        {user ?
+          <ul>
+            <li><NavLink to="/auth/logout" onClick={handleLogout}>Logout</NavLink></li>
+            {/* <li><NavLink to="/auth/change-password">Change Password</NavLink></li>  */}
+          </ul>
+        :
+          <ul>
+            <li><NavLink to="/auth/login">Log In</NavLink></li>
+            <li><NavLink to="/auth/signup">Sign Up</NavLink></li>  
+          </ul>
+        }
+      </div>
     </nav>
   )
 }
