da958cbd62936b2ce1077f6043026eec72d17ed4
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-16 16:25:05 -0400

    stub up bookservice fnction

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,6 +1,7 @@
+import CommentCard from "../CommentCard/CommentCard";
 
-
-const Comments = () => {
+const Comments = (props) => {
+  if (!props.comments.length) return <h4>No Comments</h4>
   return (
     <>
     </>
--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,2 +1,20 @@
 
+import * as tokenService from './tokenService'
 
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/books`
+
+async function createComment(blogId, commentFormData){
+  try {
+    const res = await fetch(`${BASE_URL}/${VolumeId}/comments`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(commentFormData)
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
