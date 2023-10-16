b228b03b6ab6e9288b1863456064dfa6197274f0
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-16 13:04:46 -0400

    adjustments to components

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./BookCard.module.css";
+import BookDetails from "../../pages/BookDetails/BookDetails";
 
 const BookCard = (props) => {
   return (
@@ -9,9 +10,6 @@ const BookCard = (props) => {
         <h4>{props.book.title}</h4>
         <h5>{props.book.authors}</h5>
       </Link>
-      {/* {props.onClick && (
-        <button onClick={props.onClick}>Show Details</button>
-      )} */}
     </div>
   )
 }
