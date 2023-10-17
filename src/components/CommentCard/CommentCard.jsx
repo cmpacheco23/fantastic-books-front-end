6308f211951add33c1dcf4ff66e851499f059a24
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-17 12:02:18 -0400

    add commenter name to comments

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,7 +1,7 @@
 const CommentCard = ({comment}) => {
   return ( 
     <article>
-      {/* <p>{comment.commenter}</p> */}
+      <p>{comment.commenter.name}</p>
       <p>{comment.text}</p>
       <p>{comment.rating}</p>
     </article>
