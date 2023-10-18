f9f145dca3fa18468c73f3a43a1da3ca35a4f39f
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-18 12:13:35 -0400

    stub up blog card

---
+++ src/components/BlogCard/BlogCard.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+import styles from "./BlogCard.module.css";
+
+const BlogCard = ({blog}) => {
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+export default BlogCard
