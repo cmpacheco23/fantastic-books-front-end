0691c300f237c9d927da8cb9b34fe66b4e7d5637
venmorr <stevenmorrrca@email.com>
2023-10-18 08:51:02 -0400

    work on blogs

--- src/pages/BlogList/BlogList.jsx
+++ src/pages/BlogList/BlogList.jsx
@@ -6,7 +6,12 @@ const BlogList = (props) => {
   console.log('BlogList props:', props)
   return (
     <main className={styles.container}>
-      Blog List
+      {props.blogs.map(blog => (
+        <p key={blog._id}>
+          {blog.blogTitle}
+          
+        </p>
+      ))}
     </main>
   )
 }
