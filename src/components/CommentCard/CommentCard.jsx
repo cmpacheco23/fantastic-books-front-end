42b0149f0dd4172df2b5654342522196834ad154
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-18 23:12:17 -0400

    comment card styling

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,45 +1,46 @@
 import styles from "./CommentCard.module.css"
 
 const CommentCard = (props) => {
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
-  // console.log('isEditingComment:', props.isEditingComment);
-  // console.log('comment._id:', props.comment._id);
   return (
-
     <article className={styles.commentCard}>
-      <p>{props.comment.commenter.name}</p>
-      <p>{props.comment.text}</p>
-      <p>{props.comment.rating}</p>
-      <p>{props.comment.createdAt}</p>
-      
-      
-      <div>
-
+      <div className={styles.commenterSection}>
+        {props.comment.commenter.photo && (
+          <div className={styles.namePhoto}>
+            <img
+              src={props.comment.commenter.photo}
+              alt={`Photo of ${props.comment.commenter.name}`}
+              className={styles.commenterPhoto}
+            />
+            <p className={styles.name}>{props.comment.commenter.name}</p>
+          </div>
+        )}
+      </div>
+      <div className={styles.commentText}>
+        <p>{props.comment.text}</p>
+        <p>Rating: {props.comment.rating}</p>
+        <p>Date: {formatDate(props.comment.createdAt)}</p>
+      </div>
+      <div className={styles.commentButtons}>
         <button
-          onClick={() => 
-
-            props.handleEditComment(props.volumeId, props.comment._id)}
+          onClick={() => props.handleEditComment(props.volumeId, props.comment._id)}
           disabled={props.isEditingComment === props.comment._id}
         >
-          Edit
+          ✏️
         </button>
-
         <button
           onClick={() => props.handleDeleteComment(props.volumeId, props.comment._id)}
           disabled={props.isEditingComment === props.comment._id}
         >
-          Delete
+          🗑️
         </button>
-
-
-        
       </div>
-  
-</article>
-);
+    </article>
+  );
 }
 
 export default CommentCard;
-
-
-  // { comment, volumeId, handleDeleteComment, handleEditComment, isEditingComment, formOpen }
