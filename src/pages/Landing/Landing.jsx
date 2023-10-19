384ca649d793ac85667978e4352ffe5358c083c3
venmorr <stevenmorrrca@email.com>
2023-10-19 16:35:42 -0400

    work on indentation

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -13,100 +13,72 @@ const Landing = ({ user }) => {
     </div>
   );
   return (
-    <>
-    <div className={styles.spacer}></div>
-      <main className={styles.container}>
-        <section className={styles.landing}>
+    <main className={styles.container}>
+      <div className={styles.spacer}></div>
+      <section className={styles.landing}>
+        <h1 className={styles.landingTitle}>Fantastic Books</h1>
+        <h2 className={styles.landingSubTitle}>and where to find them.</h2>
 
-          {/* {user 
-            ? 'Welcome to' 
-          : 'Log in to enjoy'
-          } */}
+        <article className="app-description">
+          <p className={styles.description}>
+            Welcome to Fantastic Books - your literary Narnia for all bibliophilic delights! Dive 20,000 leagues into a personalized reading odyssey, where you can create custom shelves to curate your tales of wonder and mystery. Whether you're charting the realms of books you've journeyed through, setting sail to the titles on your next adventure list, or unearthing niche categories from the forgotten corners of literature, our platform is your compass. Like Bilbo setting out from the Shire, embark with us and craft your unique literary epic!
+          </p>
+        </article>
+      </section>
       
-          <h1 className={styles.landingTitle}>
-              Fantastic Books
-          </h1>
-      
-          <h2 className={styles.landingSubTitle}>
-              and where to find them.
-          </h2>
-
-          <div className="app-description">
-            <p className={styles.description}>
-              Welcome to Fantastic Books - your literary Narnia for all bibliophilic delights! Dive 20,000 leagues into a personalized reading odyssey, where you can create custom shelves to curate your tales of wonder and mystery. Whether you're charting the realms of books you've journeyed through, setting sail to the titles on your next adventure list, or unearthing niche categories from the forgotten corners of literature, our platform is your compass. Like Bilbo setting out from the Shire, embark with us and craft your unique literary epic!
-            </p>
-          </div>
-        </section>
-        
-        <div className={styles.howTo}>
-
+      <section className={styles.howTo}>
         <h3>How to use this app:</h3>
-
-        <section className={styles.directions}>
-        
-        
+          <article className={styles.directions}>
             <div>
-              <h4 className="direction-title">
-              Search Book-
-              </h4>
-              <p className="direction-text">
-            Discover your next read with our intuitive search feature, powered by the Google Books API. Simply type in a title and explore the vast literary world awaiting your discovery! 
-              </p>
+              <h4 className="direction-title">Search Book-</h4>
+              <p className="direction-text">Discover your next read with our intuitive search feature, powered by the Google Books API. Simply type in a title and explore the vast literary world awaiting your discovery!</p>
             </div>
-        
+      
             <div>
-              <h4 className="direction-title">
-              Your Shelves-
-              </h4>
-              <p className="direction-text">
-            Step into the secret garden of our 'Shelf' feature – a reader's own enchanting alcove in the great library of life! Design your shelves with names as captivating as Pemberley's halls, seamlessly add tales of old and new. For those tales you wish to remain for your eyes only', simply set your shelf to private.
-              </p>
+              <h4 className="direction-title">Your Shelves-</h4>
+              <p className="direction-text">Step into the secret garden of our 'Shelf' feature – a reader's own enchanting alcove in the great library of life! Design your shelves with names as captivating as Pemberley's halls, seamlessly add tales of old and new. For those tales you wish to remain for your eyes only', simply set your shelf to private.</p>
             </div>
-        
+      
             {/* <div>
-              <h4 className="direction-title">
-              The Forum-
-              </h4>
-              <p className="direction-text">
-            Enter the Fantastic Books' Forum, our very own literary roundtable. Like Dante seeking his path, if you're unsure where your next reading journey should lead, or if you're thoughts not so gently rapping, you've found the right place.
-              </p>
+              <h4 className="direction-title">The Forum-</h4>
+              <p className="direction-text">Enter the Fantastic Books' Forum, our very own literary round table. Like Dante seeking his path, if you're unsure where your next reading journey should lead, or if you're thoughts not so gently rapping, you've found the right place.</p>
             </div> */}
-          </section>
-        </div>
-            
+        </article>
+      </section>
+          
+          
+      <section className={styles.devs}>
         <h3 className={styles.devTitle}>About the Devs:</h3>
-            
-        <section className={styles.about}>
+          <div className={styles.devCards}>
+            <article className={styles.devCard}>
+              <h2>Face</h2>
+              <h2>Carla Pacheco</h2>
+              <div className={styles.linkContainer}>
+                {renderLink(LinkedInLogo, "LinkedIn Logo", 'https://www.linkedin.com/in/thecarlapacheco/')}
+                {renderLink(GithubLogo, "GitHub Logo", 'https://github.com/cmpacheco23')}
+              </div>
+            </article>
 
-        <div>
-    <h2>Face</h2>
-    <h2>Carla Pacheco</h2>
-    <div>
-      {renderLink(LinkedInLogo, "LinkedIn Logo", 'https://www.linkedin.com/in/thecarlapacheco/')}
-      {renderLink(GithubLogo, "GitHub Logo", 'https://github.com/cmpacheco23')}
-    </div>
-  </div>
+            <article className={styles.devCard}>
+              <h2>Face</h2>
+              <h2>Enes Velovic</h2>
+              <div className={styles.linkContainer}>
+                {renderLink(LinkedInLogo, "LinkedIn Logo", 'https://www.linkedin.com/in/enesvelovic/')}
+                {renderLink(GithubLogo, "GitHub Logo", 'https://github.com/NSnyc')}
+              </div>
+            </article>
 
-  <div>
-    <h2>Face</h2>
-    <h2>Enes Velovic</h2>
-    <div>
-      {renderLink(LinkedInLogo, "LinkedIn Logo", 'https://www.linkedin.com/in/enesvelovic/')}
-      {renderLink(GithubLogo, "GitHub Logo", 'https://github.com/NSnyc')}
-    </div>
-  </div>
-
-  <div>
-    <h2>Face</h2>
-    <h2>Steve Morrison</h2>
-    <div>
-      {renderLink(LinkedInLogo, "LinkedIn Logo", 'https://www.linkedin.com/in/steven-ansman-morrison/')}
-      {renderLink(GithubLogo, "GitHub Logo", 'https://github.com/venmorr')}
-    </div>
-  </div>
-        </section>
-      </main>
-      </>
+            <article className={styles.devCard}>
+              <h2>Face</h2>
+              <h2>Steve Morrison</h2>
+              <div className={styles.linkContainer}>
+                {renderLink(LinkedInLogo, "LinkedIn Logo", 'https://www.linkedin.com/in/steven-ansman-morrison/')}
+                {renderLink(GithubLogo, "GitHub Logo", 'https://github.com/venmorr')}
+              </div>
+            </article>
+          </div>
+      </section>
+    </main>
   )
 }
 
