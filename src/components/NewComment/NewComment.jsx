afc4e35706ade02769191b2c532465332f7edd0f
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-19 14:10:44 -0400

    add a delay to handlesubmit to reopen form and a thank you for submitting message

--- src/components/NewComment/NewComment.jsx
+++ src/components/NewComment/NewComment.jsx
@@ -1,54 +1,64 @@
 import { useState } from "react"
-
 import styles from './NewComment.module.css'
 
-
 const NewComment = (props) => {
-  const [formData, setFormData] = useState({text: '', rating: 5})
+  const [formData, setFormData] = useState({ text: '', rating: 5 });
+  const [formOpen, setFormOpen] = useState(true);
 
-  const handleChange = ({target}) => {
-    setFormData({...formData, [target.name]: target.value})
+  const handleChange = ({ target }) => {
+    setFormData({ ...formData, [target.name]: target.value });
   }
 
   const handleSubmit = (evt) => {
-    evt.preventDefault()
-    props.handleAddComment(formData)
-    setFormData({text: '', rating: 5})
+    setFormOpen(false);
+    evt.preventDefault();
+    props.handleAddComment(formData);
+    setFormData({ text: '', rating: 5 });
+    const delay = 2000; 
+    setTimeout(() => {
+      setFormOpen(true);
+    }, delay);
   }
-//when i passed in state comments are now duplicating: 2pm
-  return (
-    <form className={styles.newComment} onSubmit={handleSubmit}>
-      {props.comments.length > 0 ? (
-        <h4 className={styles.commentH4Title}>Read the comments below</h4>
-      ) : (
-        <h4 className={styles.commentH4Title}>No Comments Have Been Added Yet</h4>
-      )}
-      <h3>Add New Comment</h3>
-      <label htmlFor="rating">Rating:</label>
-      <select
-        name="rating"
-        id="rating"
-        value={formData.rating}
-        onChange={handleChange}
-      >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      <textarea
-        required
-        type="text"
-        name="text"
-        value={formData.text}
-        placeholder="Add a Comment"
-        onChange={handleChange}
-      />
-      <button className={styles.submit}type="submit">Submit</button>
 
-    </form>
-  )
+  return (
+    <div>
+      {formOpen ? (
+        <form className={styles.newComment} onSubmit={handleSubmit}>
+          {props.comments.length > 0 ? (
+            <h4 className={styles.commentH4Title}>Read the comments below</h4>
+          ) : (
+            <h4 className={styles.commentH4Title}>No Comments Have Been Added Yet</h4>
+          )}
+          <h3>Add New Comment</h3>
+          <label htmlFor="rating">Rating:</label>
+          <select
+            name="rating"
+            id="rating"
+            value={formData.rating}
+            onChange={handleChange}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <textarea
+            required
+            type="text"
+            name="text"
+            value={formData.text}
+            placeholder="Add a Comment"
+            onChange={handleChange}
+          />
+          <button className={styles.submit} type="submit">Submit</button>
+        </form>
+      ) : 
+      <h2> Thanks for submitting your comment!</h2>
+      }
+      
+    </div>
+  );
 }
 
-export default NewComment
+export default NewComment;
