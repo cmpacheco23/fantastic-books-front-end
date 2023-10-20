066f45f929cccbc79dfeb67c0b9c92c605522262
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-19 23:45:16 -0400

    book card and book search styling updat es

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -3,13 +3,13 @@ import styles from "./BookCard.module.css";
 
 const BookCard = (props) => {
   return (
-    <div className={`${styles.linkContainer} ${styles.bookCardContent}`}>
+    <article className={`${styles.linkContainer} ${styles.bookCardContent}`}>
       <Link to={`/books/${props.book.id}`} className={styles.authorLink}>
         <img className={styles.bookCardImage} src={props.book.cover} alt="cover-img" />
         <h4 className={styles.bookCardText} >{props.book.title}</h4>
         <h5>{props.book.authors}</h5>
       </Link>
-    </div>
+    </article>
   )
 }
 
