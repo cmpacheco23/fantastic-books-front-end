7fc86f1c5d1616b468438017a65defabd3779ba2
venmorr <stevenmorrrca@email.com>
2023-10-20 00:28:14 -0400

    fix book search

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -6,7 +6,7 @@ const BookCard = (props) => {
     <article className={`${styles.linkContainer} ${styles.bookCardContent}`}>
       <Link to={`/books/${props.book.id}`} className={styles.authorLink}>
         <img className={styles.bookCardImage} src={props.book.cover} alt="cover-img" />
-        <h4 className={styles.bookCardText} >{props.book.title}</h4>
+        <h4 className={styles.bookCardText} >{props.book.title.length > 20 ? `${props.book.title.substring(0, 30)}...` : props.book.title}</h4>
         <h5>{props.book.authors}</h5>
       </Link>
     </article>
